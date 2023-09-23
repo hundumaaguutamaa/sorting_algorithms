13d6b63d63f05ef78f4ef531fa76dee11efef869
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2023-09-23 19:29:31 +0300

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,36 +1,38 @@
 #include "sort.h"
-/**
-* bubble_sort - Sort an array of integers. 
-* @array: Array to be sorted. 
-* @size: Size of the array. 
-*
-*/
 
+/**
+ * bubble_sort - Sort an array of integers in an ascending order.
+ * @array: The array to be sorted
+ * @size: Size of an array
+ * @Return- Array of integers
+ */
 
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int temp_val;
-	int sorted_val;
+	int temp;
+	int swap_res;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
+	for (i = 0; i < size - 1; i++)
+	{
+		swap_res = 0;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        for(j = 0; j < size - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                temp_val = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp_val;
-                print_array(array, size);
-            }
-        } 
-        if (sorted_val == 0)
-        {
-            return;
-        }   
-    }
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				swap_res = 1;
+				print_array(array, size);
+			}
+		}
+		if (swap_res == 0)
+		{
+			return;
+		}
+	}
 }
