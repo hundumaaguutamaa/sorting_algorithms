0042ce651ee01d64d1c5f58c27caff8c9f8698ca
Hunduma Tesfaye <hundumaa.guutamaa@gmail.com>
2023-09-25 12:37:13 -0700

    Insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,11 +1,36 @@
 #include "sort.h"
-
 /**
- * insertion_sort_list - Sorts a doubly linked list.
- * @list: Doubly linked list. 
- * Return-  Array of integers. 
- * 
- */
+* insertion_sort_list - Insertion sorting algorithm. 
+* @list: a linked list to sort. 
+*
+*/
 
 void insertion_sort_list(listint_t **list)
-	
+{
+    listint_t *tmp;
+    int i;
+    int swapped;
+
+    if (!list)
+        return;
+
+    do
+    {
+        swapped = 0;
+        tmp = *list;
+
+        while (tmp && tmp->next)
+        {
+            if (tmp->i > tmp->next->i)
+            {
+                i = tmp->i;
+                *(int *)&tmp->i = tmp->next->i;
+                *(int *)&tmp->next->i = i;
+                print_list(*list);
+                swapped = 1;
+            }
+            tmp = tmp->next;
+        }
+    } while (swapped);
+}
+
