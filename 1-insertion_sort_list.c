fc1ceb9587dabb5c74d137cc8749a9617c7466fa
Hunduma Tesfaye <hundumaa.guutamaa@gmail.com>
2023-09-25 10:02:21 -0700

    Insertion sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list.
+ * @list: Doubly linked list. 
+ * Return-  Array of integers. 
+ * 
+ */
+
+void insertion_sort_list(listint_t **list)
+	
